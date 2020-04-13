4937fe16b7036098de32272582cd8b8ef3e9f371
ZanyZephyr <liyizhezhe@gmail.com>
2020-04-13 17:21:08 +0800

    init

--- Example/LYZBlinkingLabel/ViewController.swift
+++ Example/LYZBlinkingLabel/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import LYZBlinkingLabel
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = LYZBlinkingLabel()
+        label.startBlinking()
     }
 
     override func didReceiveMemoryWarning() {
